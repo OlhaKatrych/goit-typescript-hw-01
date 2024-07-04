03dd83f48b2292f15b780d634fbf7b89e39d1f90
OlhaKatrych <olgak198931@gmail.com>
2024-07-04 09:52:27 +0300

    gen 1

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response: any = await axios.get(url);
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
